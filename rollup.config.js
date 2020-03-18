e10e4afba62028aae9469b93457bdb387cefcdcf
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2020-03-18 08:02:21 +0100

    Only include dist and manifest in build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 const json = require('@rollup/plugin-json');
+const copy = require('rollup-plugin-copy');
 
 module.exports.default = [
   {
@@ -19,6 +20,11 @@ module.exports.default = [
         plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h' }]],
       }),
       commonjs(),
+      copy({
+        targets: [
+          { src: 'src/style.css', dest: 'dist' },
+        ],
+      }),
     ],
   },
 ];
